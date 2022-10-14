82571086733540400987308ba673d2b35cf9265b
Robbin Höglin <r.hoglin@gmail.com>
2022-10-14 20:06:48 +0200

    first commit

---
+++ src/main/java/com/shelly/relayservice/RelayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shelly.relayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelayServiceApplication.class, args);
+	}
+
+}
